db0ad688017639538024102d88641d3ed343a132
stephaniele <tle3@macalester.edu>
2020-02-19 23:08:30 -0600

    randomly generated ID

--- LazyPlanner/LazyPlanner/Models/RecipeCategory.swift
+++ LazyPlanner/LazyPlanner/Models/RecipeCategory.swift
@@ -10,14 +10,14 @@ import Foundation
 import SwiftUI
 import UIKit
 
-struct RecipeCategory {
-    var id: Int
+struct RecipeCategory: Identifiable {
+    var id = UUID()
     var name: String
     var recipes: [Recipe]
 }
 
-var nonVeggie: RecipeCategory = RecipeCategory(id: 1, name: "Non-vegetarian", recipes: [recipeData[0],recipeData[2],recipeData[3],recipeData[5]])
+var nonVeggie: RecipeCategory = RecipeCategory(name: "Non-vegetarian", recipes: [recipeData[0],recipeData[2],recipeData[3],recipeData[5]])
 
-var vegetarian: RecipeCategory = RecipeCategory(id: 2, name: "Vegetarian", recipes: [recipeData[1],recipeData[4],recipeData[6]])
+var vegetarian: RecipeCategory = RecipeCategory(name: "Vegetarian", recipes: [recipeData[1],recipeData[4],recipeData[6]])
 
 var categoryList: [RecipeCategory] = [nonVeggie, vegetarian]
