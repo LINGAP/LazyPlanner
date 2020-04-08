c7cece38cf7a4f52c914e44f2e810fce4c7fd1cc
LINGAP <lma1169949842@gmail.com>
2020-04-07 23:35:37 -0500

    installed pod, following recommendation that not added to gitignore

---
+++ LazyPlanner/Pods/Target Support Files/Pods-LazyPlanner-LazyPlannerUITests/Pods-LazyPlanner-LazyPlannerUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LazyPlanner_LazyPlannerUITests : NSObject
+@end
+@implementation PodsDummy_Pods_LazyPlanner_LazyPlannerUITests
+@end
---
+++ LazyPlanner/Pods/Target Support Files/Pods-LazyPlanner/Pods-LazyPlanner-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LazyPlanner : NSObject
+@end
+@implementation PodsDummy_Pods_LazyPlanner
+@end
