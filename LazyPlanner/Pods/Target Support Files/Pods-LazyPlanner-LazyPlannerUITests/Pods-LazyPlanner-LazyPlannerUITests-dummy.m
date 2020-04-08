ddaeb6ba0397ab715ba0f7a9f49f7c8e5ffd2fa3
LINGAP <lma1169949842@gmail.com>
2020-04-08 10:04:13 -0500

    gitignore deleted pod

--- LazyPlanner/Pods/Target Support Files/Pods-LazyPlanner-LazyPlannerUITests/Pods-LazyPlanner-LazyPlannerUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_LazyPlanner_LazyPlannerUITests : NSObject
-@end
-@implementation PodsDummy_Pods_LazyPlanner_LazyPlannerUITests
-@end
--- LazyPlanner/Pods/Target Support Files/Pods-LazyPlanner/Pods-LazyPlanner-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_LazyPlanner : NSObject
-@end
-@implementation PodsDummy_Pods_LazyPlanner
-@end
