617c0e7351ae05e78377d39cda76dd4f310772d0
stephaniele <tle3@macalester.edu>
2020-05-06 09:46:46 -0500

    DragRecipe model

--- LazyPlanner/LazyPlanner/Models/DragRecipe.swift
+++ LazyPlanner/LazyPlanner/Models/DragRecipe.swift
@@ -7,3 +7,50 @@
 //
 
 import Foundation
+import SwiftUI
+import UIKit
+import MobileCoreServices
+
+final class DragRecipe:NSObject,NSItemProviderWriting,NSItemProviderReading,Codable{
+    let recipe:Recipe
+    let title:String
+    
+    init(recipe:Recipe){
+        self.recipe = recipe
+        self.title = recipe.title
+    }
+    static var writableTypeIdentifiersForItemProvider: [String]{
+        return [(kUTTypeData as String)]
+    }
+    static var readableTypeIdentifiersForItemProvider: [String]{
+        return [(kUTTypeData as String)]
+    }
+    
+    func loadData(withTypeIdentifier typeIdentifier: String, forItemProviderCompletionHandler completionHandler: @escaping (Data?, Error?) -> Void) -> Progress? {
+        let progress = Progress(totalUnitCount: 100)
+        do {
+            let data = try JSONEncoder().encode(self)
+            progress.completedUnitCount = 100
+            completionHandler(data,nil)
+        }
+        catch {
+            completionHandler(nil,error)
+        }
+        return progress
+    }
+    
+    
+    
+    static func object(withItemProviderData data: Data, typeIdentifier: String) throws -> DragRecipe {
+        do {
+            let subject = try JSONDecoder().decode(DragRecipe.self, from: data)
+            return subject
+        }
+        catch{
+            fatalError("\(error.localizedDescription)")
+        }
+    }
+    
+    
+}
+
